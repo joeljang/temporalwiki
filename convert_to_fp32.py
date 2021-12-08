76705c1e1288bff358378e0d3045ac3aebbb381e
Ubuntu <joel@ever-v100-1.g30ib2svvseejnu4hhqettel4b.xx.internal.cloudapp.net>
2021-12-08 05:51:26 +0000

    minor changes

--- convert_to_fp32.py
+++ convert_to_fp32.py
@@ -1,5 +1,5 @@
 from pytorch_lightning.utilities.deepspeed import convert_zero_checkpoint_to_fp32_state_dict
 # lightning deepspeed has saved a directory instead of a file
-save_path = “outputs/T5_base_ssm08lr”
-output_path = “lightning_logs/version_0/checkpoints/epoch=0-step=0_2.ckpt”
+save_path = "/home/joel/everchange-dev/outputs/T5_base_08_lr3e-5/epoch=0-step=18856.ckpt"
+output_path = "/home/joel/everchange-dev/outputs/T5_base_08_lr3e-5/epoch0.ckpt"
 convert_zero_checkpoint_to_fp32_state_dict(save_path, output_path)
